8d67313a976fdda768f4f247b6dfe29f62023927
Vyash Bhawan <vyashbhawan@gmail.com>
2025-09-30 13:54:00 +0200

    added likes placeholder playlist

--- components/LibraryPlaylistView.tsx
+++ components/LibraryPlaylistView.tsx
@@ -1,16 +1,69 @@
-import { Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import AppText from "./AppText";
+import { Ionicons } from "@expo/vector-icons";
+import { Colors } from "constants/colors";
+import { DEFAULT_ARTWORK_URI, LIKES_ICON_URI } from "constants/globalStyles";
+
+interface LibraryPlaylistViewProps {}
 
 const LibraryPlaylistView = () => {
-    return (
-        <View>
-            <Text>Library Playlist View</Text>
-            <Text>Library Playlist View</Text>
-            <Text>Library Playlist View</Text>
-            <Text>Library Playlist View</Text>
-            <Text>Library Playlist View</Text>
+  return (
+    <View style={styles.container}>
+      <View>
+        <Image style={styles.cover} source={{ uri: LIKES_ICON_URI }} />
+      </View>
+      <View style={[styles.details, { flex: 2 }]}>
+        <AppText
+          style={[styles.detailsTitle, { width: "250" }]}
+          ellipsizeMode="tail"
+          numberOfLines={1}
+        >
+          My Likes
+        </AppText>
+      </View>
+      <View
+        style={{
+          flex: 3,
+          alignItems: "flex-end",
+        }}
+      >
+        <View style={styles.actionButtons}>
+          <Ionicons
+            name="checkmark-circle-outline"
+            size={16}
+            color={Colors.text}
+          />
+          <Ionicons name="ellipsis-vertical" size={16} color={Colors.text} />
         </View>
-    )
-}
+      </View>
+    </View>
+  );
+};
 
 export default LibraryPlaylistView;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: Colors.surface,
+    gap: 10,
+    paddingVertical: 10,
+  },
+
+  cover: {
+    width: 40,
+    height: 40,
+    marginRight: 10,
+    borderRadius: 8,
+  },
+  detailsTitle: {
+    fontWeight: "bold",
+    width: "100%",
+    height: "auto",
+  },
+  actionButtons: {
+    flexDirection: "row",
+    gap: 20,
+  },
+});
