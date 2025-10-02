b3e6cce530efdb2aee61ae71bcb7cd1f165f49e4
Vyash Bhawan <vyashbhawan@gmail.com>
2025-10-02 22:22:40 +0200

    added styling create playlist button

--- components/CreatePlaylist.tsx
+++ components/CreatePlaylist.tsx
@@ -1,14 +1,38 @@
-import { Button, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import AppText from "./AppText";
 import App from "App";
 import { globalStyles } from "constants/global";
+import { Ionicons } from "@expo/vector-icons";
+import { Colors } from "constants/colors";
 
 const CreatePlaylist = () => {
   return (
-    <View>
-      <AppText>Create Playlist</AppText>
+    <View style={[styles.container]}>
+      <Text style={styles.text}>Create Playlist</Text>
+      <Ionicons
+        style={styles.icon}
+        name="add-circle-outline"
+        size={24}
+        color={Colors.primary}
+      />
     </View>
   );
 };
 
 export default CreatePlaylist;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    marginVertical: 12,
+  },
+  text: {
+    fontWeight: "bold",
+    color: Colors.text,
+  },
+  icon: {
+    marginLeft: 8,
+  },
+});
