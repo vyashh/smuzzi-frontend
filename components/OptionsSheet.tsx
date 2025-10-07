0d03c27c5b0c01c7fe8af1007ef9fcd3859488e8
Vyash Bhawan <vyashbhawan@gmail.com>
2025-10-07 23:03:41 +0200

    added close while clicking on backdrop

--- components/OptionsSheet.tsx
+++ components/OptionsSheet.tsx
@@ -1,5 +1,9 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
-import { BottomSheetModal, BottomSheetView } from "@gorhom/bottom-sheet";
+import {
+  BottomSheetBackdrop,
+  BottomSheetModal,
+  BottomSheetView,
+} from "@gorhom/bottom-sheet";
 import AppText from "./AppText";
 
 export type OptionsSheetRef = { present: () => void; dismiss: () => void };
@@ -13,7 +17,20 @@ const OptionSheet = forwardRef<OptionsSheetRef>((_, ref) => {
   }));
 
   return (
-    <BottomSheetModal ref={modalRef} snapPoints={["35%"]}>
+    <BottomSheetModal
+      ref={modalRef}
+      snapPoints={["35%"]}
+      backdropComponent={(props) => (
+        <BottomSheetBackdrop
+          {...props}
+          pressBehavior="close"
+          appearsOnIndex={0}
+          disappearsOnIndex={-1}
+        />
+      )}
+      enableDynamicSizing
+      enablePanDownToClose
+    >
       <BottomSheetView>
         <AppText>Option Sheet</AppText>
       </BottomSheetView>
