bd02885e659f7a0b4df621fa0c590ad70987d012
Vyash Bhawan <vyashbhawan@gmail.com>
2025-10-21 23:49:16 +0200

    changed card styling

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,8 +1,14 @@
-import { FlatList, ImageBackground, Pressable, StyleSheet } from "react-native";
+import {
+  FlatList,
+  Image,
+  ImageBackground,
+  Pressable,
+  StyleSheet,
+} from "react-native";
 import { Song } from "types/song";
 import AppText from "./AppText";
-import { LinearGradient } from "expo-linear-gradient";
 import { DEFAULT_ARTWORK_URI } from "constants/global";
+import { Colors } from "constants/colors";
 
 interface CardProps {
   data: Song[];
@@ -22,33 +28,25 @@ const Card = ({ data, handleTrackPress }: CardProps) => {
             onPress={() => handleTrackPress?.(song["track_id"])}
             style={styles.container}
           >
-            <ImageBackground
+            <Image
               style={styles.image}
               source={{ uri: song["cover_url"] || DEFAULT_ARTWORK_URI }}
               resizeMode="cover"
+            />
+            <AppText
+              style={styles.title}
+              numberOfLines={1}
+              ellipsizeMode="tail"
             >
-              <LinearGradient
-                colors={["#16182a21", "#0C0D13"]}
-                start={{ x: 0, y: 0 }}
-                end={{ x: 0.5, y: 2 }}
-                style={styles.gradient}
-              >
-                <AppText
-                  style={styles.text}
-                  numberOfLines={1}
-                  ellipsizeMode="tail"
-                >
-                  {song.artist || "Artist"}
-                </AppText>
-                <AppText
-                  style={styles.text}
-                  numberOfLines={1}
-                  ellipsizeMode="tail"
-                >
-                  {song.title || "Title"}
-                </AppText>
-              </LinearGradient>
-            </ImageBackground>
+              {song.title || "Title"}
+            </AppText>
+            <AppText
+              style={styles.artist}
+              numberOfLines={1}
+              ellipsizeMode="tail"
+            >
+              {song.artist || "Artist"}
+            </AppText>
           </Pressable>
         );
       }}
@@ -60,24 +58,26 @@ export default Card;
 
 const styles = StyleSheet.create({
   container: {
-    height: 150,
+    // height: 150,
     width: 100,
     marginRight: 10,
     marginTop: 10,
   },
   image: {
     flex: 1,
+    height: 100,
+    width: 100,
     borderRadius: 16,
     overflow: "hidden",
-    justifyContent: "center",
+    marginBottom: 10,
   },
-  gradient: {
-    ...StyleSheet.absoluteFillObject,
-    justifyContent: "flex-end",
-    padding: 8,
-  },
-  text: {
-    // color: Colors.primary,
+  artist: {
     fontWeight: "bold",
+    fontSize: 12,
+    opacity: 0.5,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 12,
   },
 });
