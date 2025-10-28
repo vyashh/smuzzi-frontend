c442a7c2f30bb43bc0181fff4a86a614bdad7004
Vyash Bhawan <vyashbhawan@gmail.com>
2025-10-28 17:34:39 +0100

    fixed navigation bug where library view would route back to the wrong page

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,15 +1,28 @@
 import { Ionicons } from "@expo/vector-icons";
+import { PlaylistType } from "constants/global";
 import { router } from "expo-router";
 import { Pressable, StyleSheet, View } from "react-native";
 
 interface TopBarProps {
   showOptions?: boolean;
   onOptionsPress?: () => void;
+  viewType?: PlaylistType;
 }
 
-const TopBar = ({ showOptions = false, onOptionsPress }: TopBarProps) => {
+const TopBar = ({
+  showOptions = false,
+  onOptionsPress,
+  viewType,
+}: TopBarProps) => {
   const handleOnPress = () => {
-    router.back();
+    // needed since navigating from home to a playlist bugs out
+    // when you then navigate to library through the tab bar it will be on the playlist view
+    // going back then brings you back to home instead of the view with all the playlists
+    if (viewType == "playlist") {
+      router.push("library");
+    } else {
+      router.back();
+    }
   };
   return (
     <View style={styles.container}>
